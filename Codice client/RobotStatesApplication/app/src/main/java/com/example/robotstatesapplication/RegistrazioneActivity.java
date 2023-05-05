6c64f079f20b005e35602b5d787e8acbfafebc72
Ferdinando <fer.dalessandro@studenti.unina.it>
2023-05-05 18:31:35 +0200

    Aggiunta interfaccia registrazione (bozza)

---
+++ Codice client/RobotStatesApplication/app/src/main/java/com/example/robotstatesapplication/RegistrazioneActivity.java
@@ -0,0 +1,16 @@
+package com.example.robotstatesapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.robotstatesapplication.R;
+
+public class RegistrazioneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrazione);
+    }
+}
