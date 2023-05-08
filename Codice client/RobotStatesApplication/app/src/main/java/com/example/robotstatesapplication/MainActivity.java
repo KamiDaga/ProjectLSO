13e8ed20bc95da803ced061b11ea4b8e45786166
Ferdinando <fer.dalessandro@studenti.unina.it>
2023-05-08 11:32:08 +0200

    Rifinitura prime interfacce

--- Codice client/RobotStatesApplication/app/src/main/java/com/example/robotstatesapplication/MainActivity.java
+++ Codice client/RobotStatesApplication/app/src/main/java/com/example/robotstatesapplication/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.robotstatesapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,9 +18,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView textViewRegistrazione = findViewById(R.id.textViewInizioRegistrazione);
+        TextView bottoneRegistrazione = findViewById(R.id.textViewInizioRegistrazione);
+        Button bottoneLogin = findViewById(R.id.bottoneLogin);
 
         SpannableStringBuilder str = new SpannableStringBuilder("Non hai un account? Registrati qui");
         str.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD), 20, 34, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         textViewRegistrazione.setText(str);
+
+        bottoneRegistrazione.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, RegistrazioneActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- Codice client/RobotStatesApplication/app/src/main/java/com/example/robotstatesapplication/RegistrazioneActivity.java
+++ Codice client/RobotStatesApplication/app/src/main/java/com/example/robotstatesapplication/RegistrazioneActivity.java
@@ -2,15 +2,82 @@ package com.example.robotstatesapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-import com.example.robotstatesapplication.R;
+import java.util.Calendar;
 
 public class RegistrazioneActivity extends AppCompatActivity {
 
+    private Spinner spinnerSesso;
+    private Button bottoneAvanti, bottoneIndietro;
+    private ImageView bottoneCalendario;
+    private DatePickerDialog datePicker;
+    private TextView dateView;
+    private Calendar calendario = Calendar.getInstance();
+
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrazione);
+
+        spinnerSesso = findViewById(R.id.spinnerSessoRegistrazione);
+        bottoneAvanti = findViewById(R.id.bottoneInizioQuestionario);
+        bottoneIndietro = findViewById(R.id.bottoneAnnullaRegistrazione);
+        bottoneCalendario = findViewById(R.id.iconaCalendarioRegistrazione);
+        dateView = findViewById(R.id.editTextDataNascita);
+
+        String[] sessi = new String[]{"M", "F"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(RegistrazioneActivity.this, R.layout.spinner_layout, sessi);
+        adapter.setDropDownViewResource(R.layout.spinner_item_layout);
+        spinnerSesso.setAdapter(adapter);
+
+        Calendar calendar = Calendar.getInstance();
+        mostraData();
+        datePicker = new DatePickerDialog(RegistrazioneActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendario.set(year, month, dayOfMonth);
+                mostraData();
+            }
+        }, calendario.get(Calendar.YEAR), calendario.get(Calendar.MONTH), calendario.get(Calendar.DAY_OF_MONTH));
+
+        bottoneAvanti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(RegistrazioneActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        bottoneIndietro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(RegistrazioneActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        bottoneCalendario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePicker.show();
+            }
+        });
+
+    }
+
+    private void mostraData() {
+        dateView.setText(new StringBuilder().append(calendario.get(Calendar.DAY_OF_MONTH)).append(" / ").append(calendario.get(Calendar.MONTH)).append(" / ").append(calendario.get(Calendar.YEAR)));
     }
 }
